b8a36330ca6ac5d685ff1ab23c63d780adb0b534
Isaiah <isaiah.leiva@gmail.com>
2022-02-19 13:31:19 -0700

    commit

--- app/Controllers/ListsController.js
+++ app/Controllers/ListsController.js
@@ -4,7 +4,9 @@ import { ProxyState } from "../AppState.js";
 //Private
 function _drawList() {
     let template = ''
-
+    const lists = ProxyState.lists
+    lists.forEach(l => template += l.Template)
+    document.getElementById('lists').innerHTML = template
 }
 
 
@@ -12,15 +14,27 @@ function _drawList() {
 export class ListsController {
     constructor() {
         ProxyState.on('lists', _drawList)
-        ProxyState.on('tasks', _drawTask)
-        _draw()
+        ProxyState.on('tasks', _drawList)
+        ProxyState.on('lists', saveState)
+        ProxyState.on('tasks', saveState)
+
+        loadState()
     }
 
     createList() {
         window.event.preventDefault()
         const form = window.event.target
+
+        const rawList = {
+            name: form.listname.value,
+            color: form.listcolor.value
+        }
+        listsService.createList(rawList)
     }
 
+    deleteList(id) {
+        listsService.deleteList(id)
+    }
 
 }
 
